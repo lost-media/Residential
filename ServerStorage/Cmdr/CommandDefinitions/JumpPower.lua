16150a31518fe53c13a272f4d25a360a22d9b12e
Brandon <kongbrandon0@gmail.com>
2024-06-27 01:17:50 -0500

    feat: added jumppower cmd

---
+++ ServerStorage/Cmdr/CommandDefinitions/JumpPower.lua
@@ -0,0 +1,20 @@
+-- ClearPlot.lua
+
+return {
+	Name = "jumppower",
+	Aliases = { "jp" },
+	Description = "Sets the jump power of the specified player",
+	Group = "Admin",
+	Args = {
+		{
+			Type = "player",
+			Name = "player",
+			Description = "The player whose jump power you want to set",
+		},
+        {
+            Type = "number",
+            Name = "power",
+            Description = "The power of the jump",
+        },
+	},
+}
---
+++ ServerStorage/Cmdr/CommandDefinitions/JumpPowerServer.lua
@@ -0,0 +1,16 @@
+-- JumpPowerServer.lua
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+---@type LMEngineServer
+local LMEngine = require(ReplicatedStorage.LMEngine)
+
+-- These arguments are guaranteed to exist and be correctly typed.
+return function(context, player: Player, jumpPower: number)
+	if (player.Character ~= nil) then
+        player.Character.Humanoid.UseJumpPower = true;
+        player.Character.Humanoid.JumpPower = jumpPower;
+
+        return "Changed jump power for " .. player.Name
+    end
+end
